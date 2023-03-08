f36d3729ec9b1ebd093c8f8045401657f608f545
Belkacem Oussama <oussama.blkc@gmail.com>
2023-03-08 18:24:08 +0100

    stack okay, have to add text behind of logo

--- src/components/Presentation/Presentation.js
+++ src/components/Presentation/Presentation.js
@@ -1,13 +1,16 @@
 import { datas } from '../../data/data.js'
+import Card from 'react-bootstrap/Card'
 
 import dev from '../../assets/dev.jpg'
 import jjb from '../../assets/jjb.jpg'
 import football from '../../assets/football.jpg'
 import travel from '../../assets/travel.jpg'
+import reactJs from '../../assets/reactJs.png'
+import jsLogo from '../../assets/js.png'
+import phpLogo from '../../assets/php.png'
+import symfonyLogo from '../../assets/symfony.png'
 
 import styles from './styles.css'
-import { useRef, useState } from 'react'
-
 
 export default function Presentation() {
 
@@ -31,7 +34,121 @@ export default function Presentation() {
             <img className='dev_assets' src={travel}></img>
           </div>
         </div>
-        <div className='presentation_stack'>Stack</div>
+
+        <div className='presentation_stack'>
+          <h2>J'aime me former sur de nouvelles technos !</h2>
+          <ul>
+            <li>
+              <Card style={{ 
+                width: '20rem', 
+                backgroundColor: '#118a7e',
+                position: 'relative',
+                border: 'none'
+              }}>
+                <Card.Img 
+                  src={reactJs}
+                  style={{ 
+                    width: '13rem', 
+                    backgroundColor: '#118a7e',
+                    transformStyle: 'preserve-3d',
+                    transition: 'transform 1.5s',
+                    backfaceVisibility: 'hidden',
+                    cursor: 'pointer'
+                  }}
+                  onMouseOver={(e) => {
+                    e.currentTarget.style.transform = 'rotateY(180deg)';
+                  }}
+
+                  onMouseOut={(e) => {
+                    e.currentTarget.style.transform = 'rotateY(0deg)';
+                  }}
+                />
+              </Card>
+            </li>
+            <li>
+              <Card style={{ 
+                width: '20rem', 
+                backgroundColor: '#118a7e',
+                position: 'relative',
+                border: 'none'
+              }}>
+                <Card.Img 
+                  src={jsLogo}
+                  style={{ 
+                    width: '13rem', 
+                    backgroundColor: '#118a7e',
+                    transformStyle: 'preserve-3d',
+                    transition: 'transform 1.5s',
+                    backfaceVisibility: 'hidden',
+                    cursor: 'pointer'
+                  }}
+                  onMouseOver={(e) => {
+                    e.currentTarget.style.transform = 'rotateY(180deg)';
+                  }}
+
+                  onMouseOut={(e) => {
+                    e.currentTarget.style.transform = 'rotateY(0deg)';
+                  }}
+                />
+              </Card>
+            </li>
+            <li>
+              <Card style={{ 
+                width: '20rem', 
+                backgroundColor: '#118a7e',
+                position: 'relative',
+                border: 'none'
+              }}>
+                <Card.Img 
+                  src={phpLogo}
+                  style={{ 
+                    width: '13rem', 
+                    backgroundColor: '#118a7e',
+                    transformStyle: 'preserve-3d',
+                    transition: 'transform 1.5s',
+                    backfaceVisibility: 'hidden',
+                    cursor: 'pointer'
+                  }}
+                  onMouseOver={(e) => {
+                    e.currentTarget.style.transform = 'rotateY(180deg)';
+                  }}
+
+                  onMouseOut={(e) => {
+                    e.currentTarget.style.transform = 'rotateY(0deg)';
+                  }}
+                />
+              </Card>
+            </li>
+            <li>
+              <Card style={{ 
+                width: '20rem', 
+                backgroundColor: '#118a7e',
+                position: 'relative',
+                border: 'none'
+              }}>
+                <Card.Img 
+                  src={symfonyLogo}
+                  style={{ 
+                    width: '13rem', 
+                    backgroundColor: '#118a7e',
+                    transformStyle: 'preserve-3d',
+                    transition: 'transform 1.5s',
+                    backfaceVisibility: 'hidden',
+                    cursor: 'pointer'
+                  }}
+                  onMouseOver={(e) => {
+                    e.currentTarget.style.transform = 'rotateY(180deg)';
+                  }}
+
+                  onMouseOut={(e) => {
+                    e.currentTarget.style.transform = 'rotateY(0deg)';
+                  }}
+                />
+              </Card>
+            </li>
+          </ul>
+        </div>
+
         <div className='presentation_hobbies'>Hobbies</div>
         <div className='presentation_projects'>Projects</div>
         <div className='presentation_else'>Else</div>
--- src/data/data.js
+++ src/data/data.js
@@ -2,7 +2,6 @@ export const datas = [
     {
         title: 'Welcome on my personal page !',
         introduction: "Dev', Jiu Jitsu Brésilien, Football, Voyage, ... ENCHANTÉ !",
-        stack: 'stack',
         hobbies: 'hobbies',
     }
 ]
