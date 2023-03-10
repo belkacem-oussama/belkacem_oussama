b2ab8e9c3f38aa11ef746f85fad2b7036108abc2
Belkacem Oussama <oussama.blkc@gmail.com>
2023-03-10 12:27:10 +0100

    itube + wtm cards okay

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,8 @@ import styles from './styles.css'
 import Card from 'react-bootstrap/Card';
 import jkpLogo from '../../assets/jankenpon.jpg'
 import tdlLogo from '../../assets/todolist.jpg'
+import itubeLogo from '../../assets/itube.jpg'
+import wtmLogo from '../../assets/wtm.jpg'
 import { datas } from '../../data/data';
 
 
@@ -12,6 +14,8 @@ export default function Projects() {
 
     const projectJKP = projectData.map((data) => data.description_JKP)
     const projectTDL = projectData.map((data) => data.description_TDL)
+    const projectItube = projectData.map((data) => data.description_Itube)
+    const projectWTM = projectData.map((data) => data.description_WTM)
 
   return (
     <div className='projects'>
@@ -32,6 +36,26 @@ export default function Projects() {
             <div className='projects_first_text'>
                 <h1>TodoList (...who didn't code it ?)</h1>
                 <p>{projectTDL}</p>
+                <a href='#' target="_blank">You can try it here ! </a>
+            </div>
+        </div>
+        <div className='projects_first'>
+            <div className='projects_first_pic'>
+                <img src={itubeLogo}></img>
+            </div>
+            <div className='projects_first_text'>
+                <h1>Itube ! The clone ! (...or not)</h1>
+                <p>{projectItube}</p>
+                <a href='#' target="_blank">You can try it here ! </a>
+            </div>
+        </div>
+        <div className='projects_first'>
+            <div className='projects_first_pic'>
+                <img src={wtmLogo}></img>
+            </div>
+            <div className='projects_first_text'>
+                <h1>What's the movie ?</h1>
+                <p>{projectWTM}</p>
                 <a href='#' target="_blank">You can try it here ! </a>
             </div>
         </div>
--- src/data/data.js
+++ src/data/data.js
@@ -6,6 +6,8 @@ export const datas = [
 
     {
         description_JKP: 'Here is my first project ! A little game made with JS.Vanilla, I still work on it adding some styles...',
-        description_TDL: 'Here an other project ! A Todolist, a lot of people made it ! I still adding styles...'
+        description_TDL: 'Here an other project ! A Todolist, a lot of people made it ! I still adding styles...',
+        description_Itube: 'Finally here is my last alone project ! Made with React and Material UI, I get datas from YoutubeAPI and tried to do something, still working on !',
+        description_WTM: 'Here is a project made with a team of Front-Dev and Back-Dev, it is a game where you have to find the movies with hints. I let you discover the features testing it !',
     }
 ]
