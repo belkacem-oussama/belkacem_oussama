5ae4947d437e34ee9c6659800fcf8afd6aef4193
Belkacem Oussama <oussama.blkc@gmail.com>
2023-03-10 15:17:44 +0100

    fix styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Coding from '../Coding/Coding'
 import Home from '../Home/Home'
 
 export default function App() {
   return (
     <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/coding' element={<Coding />} />
     </Routes>
   )
 }
---
+++ src/components/Coding/Coding.js
@@ -0,0 +1,10 @@
+
+export default function Coding() {
+  return (
+    <div className='presentation'>
+        <div className='presentation_parcours'>
+            ekke
+        </div>
+    </div>
+  )
+}
