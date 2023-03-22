04ee07b32f838422e18f495f3a2de4063e48c26f
Belkacem Oussama <oussama.blkc@gmail.com>
2023-03-22 18:03:53 +0100

    try to add fliped card

--- src/components/Stack/Card/Card.js
+++ src/components/Stack/Card/Card.js
@@ -1,34 +1,82 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import styls from './styles.css'
 
 function BasicExample(props) {
 
   const { title, text, image } = props
 
+  const [showMore, setShowMore] = useState(false)
+
+  const handleShowMore = () => {
+    setShowMore(!showMore)
+    console.log(showMore);
+  }
+
   return (
-    <Card 
-    style={{
-       width: '30rem', 
-       border:'solid 2px grey', 
-       padding:'10px', 
-       borderRadius:'10px' 
-       }}
-    >
-      <Card.Img variant="top" src={image} style={{width:'100%'}} />
-      <Card.Body>
-        <Card.Title className='card_title' >{title}</Card.Title>
-        <Card.Text className='card_text' >{text}</Card.Text>
-        <Button variant="primary" className='card_button' >Show more !</Button>
-      </Card.Body>
-    </Card>
+    <div className='flip-card'>
+      <div className='flip-card-inner'>
+        <div className='flip-card-front'>
+          <Card 
+          style={{
+            width: '30rem', 
+            border:'solid 2px grey', 
+            padding:'10px', 
+            borderRadius:'10px' 
+            }}
+          >
+            <Card.Img variant="top" src={image} style={{width:'100%'}} />
+            <Card.Body>
+              <Card.Title className='card_title' >{title}</Card.Title>
+              <Card.Text className='card_text' >{text}</Card.Text>
+              <Button 
+                variant="primary" 
+                className='card_button' 
+                onClick={handleShowMore}
+              >
+                Show more !
+              </Button>
+            </Card.Body>
+          </Card>
+          {
+            showMore ? 
+          <div className='flip-card-back'>
+          <Card 
+          style={{
+            width: '30rem', 
+            border:'solid 2px grey', 
+            padding:'10px', 
+            borderRadius:'10px' 
+            }}
+          >
+            <Card.Img variant="top" src={image} style={{width:'100%'}} />
+            <Card.Body>
+              <Card.Title className='card_title' >{title}</Card.Title>
+              <Card.Text className='card_text' >{text}</Card.Text>
+              <Button 
+                variant="primary" 
+                className='card_button' 
+                onClick={handleShowMore}
+              >
+                Show more !
+              </Button>
+            </Card.Body>
+          </Card>
+          </div>
+          :
+          null
+          }   
+        </div>
+      </div>
+    </div>
   );
 }
 
 BasicExample.propTypes = {
   title: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
-  buttonText: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
 };
 
--- src/components/Stack/Stack.js
+++ src/components/Stack/Stack.js
@@ -1,17 +1,18 @@
-import React from 'react'
 import Card from './Card/Card.js'
 import styles from './styles.css'
 import frontLogo from '../../assets/front.jpg'
 import backLogo from '../../assets/back.jpg'
 import toolLogo from '../../assets/tools.jpg'
 
+
 export default function Stack() {
+
   return (
     <div className='cards'>
       <Card 
         title='Front-End'
         image={frontLogo}
-        text= 'For the interaction !' 
+        text= 'For the interaction !'
       />
       <Card 
         title='Back-End'
