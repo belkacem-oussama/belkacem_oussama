88c3b57aa1fad419e4cdd0ba5370261317488ce4
Belkacem Oussama <oussama.blkc@gmail.com>
2023-03-22 17:03:39 +0100

    more style for card component

--- src/components/Stack/Card/Card.js
+++ src/components/Stack/Card/Card.js
@@ -7,12 +7,19 @@ function BasicExample(props) {
   const { title, text, image } = props
 
   return (
-    <Card style={{ width: '18rem' }}>
+    <Card 
+    style={{
+       width: '30rem', 
+       border:'solid 2px grey', 
+       padding:'10px', 
+       borderRadius:'10px' 
+       }}
+    >
       <Card.Img variant="top" src={image} style={{width:'100%'}} />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{text}</Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary">Show more !</Button>
       </Card.Body>
     </Card>
   );
--- src/components/Stack/Stack.js
+++ src/components/Stack/Stack.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import Card from './Card/Card.js'
 import styles from './styles.css'
+import frontLogo from '../../assets/front.jpg'
+import backLogo from '../../assets/back.jpg'
+import toolLogo from '../../assets/tools.jpg'
 
 export default function Stack() {
   return (
     <div className='cards'>
-      <Card />
+      <Card 
+        title='Front-End'
+        image={frontLogo}
+        text= 'For the interaction !' 
+      />
+      <Card 
+        title='Back-End'
+        image={backLogo}
+        text= 'For the server !' 
+      />
+      <Card 
+        title='Front-End'
+        image={toolLogo}
+        text= 'For the interaction !' 
+      />
     </div>
   )
 }
