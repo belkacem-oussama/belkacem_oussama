66775f332aa7eeb444f9226983272cd2f0a401f8
Belkacem Oussama <oussama.blkc@gmail.com>
2023-03-25 13:46:28 +0100

    style for project

--- src/components/Projects/Project/Project.js
+++ src/components/Projects/Project/Project.js
@@ -7,13 +7,14 @@ const projectDatas = datas
 
   return (
         projectDatas.map(project => (
-            <div className='projects_first'>
-                <div className='projects_first_pic'>
+            <div className='projects_card'>
+                <div className='projects_card_pic'>
                     <img src={project.picture}></img>
                 </div>
-                <div className='projects_first_text'>
+                <div className='projects_card_text'>
                     <h1>{project.title}</h1>
                     <p>{project.description}</p>
+                    <p>{project.tech}</p>
                     <a href={project.link} target="_blank">You can try it here !</a>
                 </div>
             </div>
--- src/data/data.js
+++ src/data/data.js
@@ -6,26 +6,30 @@ import wtmLogo from '../assets/wtm.jpg'
 export const datas = [
     {
         title: 'Jan-Ken-Pon !',
-        description:'Here is my first project ! A little game made with JS.Vanilla, I still work on it adding some styles...',
+        description:'A little game !',
         link: '',
+        tech: 'Vanilla JS',
         picture: jpkLogo
     },
     {
-        title: 'TodoList (...who didn\'t code it ?)',
-        description:'Here is an other project ! A Todolist, a lot of people made it ! I still adding styles...',
-        link: '',
-        picture: tdlLogo
-    },
-    {
         title: 'Itube',
-        description:'Finally here is my last alone project ! Made with React and Material UI, I get datas from YoutubeAPI and tried to do something, still working on !',
+        description:'Thanks to Google API !',
         link: '',
+        tech: 'React, MUI',
         picture: itubeLogo
     },
     {
-        title: 'What\'s the movie ?',
-        description:'Here is a project made with a team of Front-Dev and Back-Dev, it is a game where you have to find the movies with hints. I let you discover the features testing it !',
+        title: 'TodoList',
+        description:'The famous !',
         link: '',
+        tech: 'React',
+        picture: tdlLogo
+    },
+    {
+        title: 'What\'s the movie ?',
+        description:'A team project !',
+        link: '',
+        tech: 'React, Redux, Symfony, Bootstrap',
         picture: wtmLogo
     },
 ]
