aac9543b9c5d658921242a229dad80a43fa84704
Belkacem Oussama <oussama.blkc@gmail.com>
2023-03-26 16:33:23 +0200

    working on presentationPage

--- src/components/PresentationPage/PresentationPage.js
+++ src/components/PresentationPage/PresentationPage.js
@@ -1,6 +1,12 @@
 import { presentationText } from '../../data/presentation'
 import styles from './styles.css'
 
+import codeLogo from '../../assets/code.jpg'
+import linkedInLogo from '../../assets/linkedin.png'
+import gmailLogo from '../../assets/gmail.png'
+import twitterLogo from '../../assets/twitter.png'
+import gitHubLogo from '../../assets/github.png'
+
 export default function PresentationPage() {
 
     const presentationData = presentationText
@@ -9,11 +15,20 @@ export default function PresentationPage() {
       <div className='text_div'>
           {presentationData.map(element => (
             <div className='text_introduction'>
-                <h1>Me !</h1>
-                <p>{element.introduction}</p>
+              <div className='text_introduction_text'>
+              <h1>Me !</h1>
+                <p>{element.introduction} &#128516;</p>
                 <a href={element.formation_link} target="_blank">Formation suivie chez O'clock !</a>
+              </div>
+              <div className='text_introduction_socials'>
+                <a href='https://www.linkedin.com/in/oussama-belkacem-767717196/' target='_blank'><img src={linkedInLogo}></img></a>
+                <a href='mailto:oussama.blkc@gmail.com'><img src={gmailLogo}></img></a>
+                <a href='https://twitter.com/ob__tech' target='_blank'><img src={twitterLogo}></img></a>
+                <a href='https://github.com/belkacem-oussama' target='_blank'><img src={gitHubLogo}></img></a>
+              </div>
             </div>
             ))}
+            <div className='text_pic'><img src={codeLogo}></img></div>
       </div>
   )
 }
--- src/data/presentation.js
+++ src/data/presentation.js
@@ -1,6 +1,6 @@
 export const presentationText = [
     {
-        introduction : "Bon… un peu de français ici… Salut à toi ! Je me présente, je m’appelle Oussama, ex-comptable de 25 ans souhaitant changer de carrière.Je me suis formé au développement web par l’apprentissage de différents langages (mais bon, tu as sûrement déjà lu ma stack donc pas la peine que je te cite tout…) afin de changer de vie et afin de progresser professionnellement mais surtout humainement.J'essaie de devenir chaque jour une meilleure personne que la veille et j’ai souhaité commencer ce challenge par un changement de carrière professionnelle !Hmm je pense que c’est déjà pas mal, du coup si tu veux discuter tu as mon LinkedIn en bas de page ou tu peux m’envoyer un message directement via le site ! Allez à plus tard !",
+        introduction : "Bon… un peu de français ici… Salut à toi ! Je me présente, je m’appelle Oussama, ex-comptable de 25 ans souhaitant changer de carrière. Je me suis formé au développement web par l’apprentissage de différentes technologies afin de changer de vie et afin de progresser professionnellement mais surtout humainement. Basé à Senlis dans l'Oise, j'attends un signe de ta part ! à bientot",
         formation_link: 'https://oclock.io/formations/developpeur-web',
     }
 ]
