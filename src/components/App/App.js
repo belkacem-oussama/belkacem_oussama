364bc1546a277549f7d5f86c35358a595800c8aa
Belkacem Oussama <oussama.blkc@gmail.com>
2023-03-28 17:38:25 +0200

    add splash screen

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
+import { useState } from 'react'
 // import Contact from '../Contact/Contact'
 import Error from '../Error/Error'
 import Header from '../Header/Header'
@@ -10,17 +11,29 @@ import Stack from '../Stack/Stack'
 import styles from './styles.scss'
 
 export default function App() {
+  const[loading, setLoading] = useState(true)
+  const root = document.getElementById('spinner')
+
+  if(root){
+    setTimeout(()=> {
+      root.style.display = "none";
+      setLoading(false)
+    },2000)
+  }
+
   return (
-    <div className='app'>
-      <Header />
-      <Routes>
-          <Route path='*' element={<Error />} />
-          <Route path='/' element={<Home />} />
-          <Route path='/stack' element={<Stack />} />
-          <Route path='/projects' element={<Projects />} />
-          {/* <Route path='/contact' element={<Contact />} /> */}
-          <Route path='/presentation' element={<PresentationPage />} />
-      </Routes>
-    </div>
+    !loading &&(
+      <div className='app'>
+        <Header />
+        <Routes>
+            <Route path='*' element={<Error />} />
+            <Route path='/' element={<Home />} />
+            <Route path='/stack' element={<Stack />} />
+            <Route path='/projects' element={<Projects />} />
+            {/* <Route path='/contact' element={<Contact />} /> */}
+            <Route path='/presentation' element={<PresentationPage />} />
+        </Routes>
+      </div>
+    )
   )
 }
