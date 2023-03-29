0fcf64b0f9b39e350404bd73d60f9eefda4cba0f
Belkacem Oussama <oussama.blkc@gmail.com>
2023-03-29 19:03:30 +0200

    modify home message automatically + styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,35 @@
+import { useState, useEffect } from 'react'
 import styles from './styles.scss'
 
 export default function Home() {
 
+  const [welcomeMessageIndex, setWelcomeMessageIndex] = useState(0)
+
+  const welcomeMessageArray = [
+    'Welcome on my personal page !',
+    'Bienvenue sur ma page personnelle !',
+    '¡Bienvenido a mi página personal !',
+    'Willkommen auf meiner persönlichen Seite!',
+    '私の個人ページへようこそ！',
+    'أهلاً وسهلاً في صفحتي الشخصية!',
+  ]
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setWelcomeMessageIndex((welcomeMessageIndex + 1) % welcomeMessageArray.length)
+    }, 5000)
+ 
+    return() => {
+      clearTimeout(timerId)
+    }
+  }, [welcomeMessageIndex])
+
+  const welcomeMessage = welcomeMessageArray[welcomeMessageIndex]
+
   return (
       <div className='presentation'>
           <div className='presentation_title'>
-            <h1 className='title'>Welcome on my personal page !</h1>
+            <h1 className='title'>{welcomeMessage}</h1>
           </div>
           <p>(click top left to navigate...)</p>
       </div>
